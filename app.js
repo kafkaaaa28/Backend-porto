3886e96a9bbac723143266b20d20704c14a19a66
kafkaaaa28 <kafkafj1@gmail.com>
2025-04-27 19:51:14 +0700

    tes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const pesanRoutes = require('./routes/messageRoutes.js');
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://192.168.100.230:3000', 'http://localhost:3000', 'https://www.kafkaaaa.my.id', 'https://kafkaaaa.my.id'],
+    origin: ['http://192.168.100.230:3000', 'http://localhost:3000', 'https://www.kafkaaaa.my.id', 'https://kafkaaaa.my.id', 'https://portofolio-puce-delta.vercel.app'],
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
     credentials: true,
   })
