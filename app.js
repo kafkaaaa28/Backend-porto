f7e66094a8615a87cd4681ceba79f8145e3c4d53
kafkaaaa28 <kafkafj1@gmail.com>
2025-04-27 21:27:17 +0700

    cors

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const authRoutes = require('./routes/authRoutes.js');
 const pesanRoutes = require('./routes/messageRoutes.js');
 app.use(express.json());
 // tes
-app.use(
-  cors({
-    origin: ['http://192.168.100.230:3000', 'http://localhost:3000', 'https://www.kafkaaaa.my.id', 'https://kafkaaaa.my.id', 'https://portofolio-puce-delta.vercel.app'],
-    methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://www.kafkaaaa.my.id', 'https://kafkaaaa.my.id', 'https://portofolio-puce-delta.vercel.app'],
+  methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
+  credentials: true,
+};
 
+// 🔥 Middleware untuk semua request
+app.use(cors(corsOptions));
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/pesan', pesanRoutes);
