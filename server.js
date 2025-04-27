8ee51c2b2c4c04c31303eef2a79d1d2538b9c6fc
kafkaaaa28 <kafkafj1@gmail.com>
2025-04-27 20:49:50 +0700

    port 8080

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 require('dotenv').config();
 const app = require('./app');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server jalan di port ${PORT}`);
