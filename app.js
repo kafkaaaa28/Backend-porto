703319d34d41918deebf11d726daa882d143b1a4
kafkaaaa28 <kafkafj1@gmail.com>
2025-04-27 17:32:52 +0700

    tes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes.js');
 const pesanRoutes = require('./routes/messageRoutes.js');
 app.use(express.json());
-
+// buat frontend
 app.use(
   cors({
     origin: ['http://192.168.100.230:3000', 'http://localhost:3000', 'http://192.168.100.230:5000', 'https://kafkaaaa.my.id'],
